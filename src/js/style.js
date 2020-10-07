c05782259dccd90924b186fcae8e9ef8642d07cd
timkeane <t.d.keane@gmail.com>
2020-10-07 14:21:22 -0400

    fixes issue #5

--- src/js/style.js
+++ src/js/style.js
@@ -5,9 +5,9 @@ export default (feature, resolution) => {
   return new Style({
     fill: new Fill({
       color: {
-        red: 'rgba(255,0,0,.7)',
-        orange: 'rgba(255,165,0,.7)',
-        yellow: 'rgba(240,255,0,.7)'
+        red: 'rgba(255,18,0,.7)',
+        orange: 'rgba(255,102,0,.7)',
+        yellow: 'rgba(255,255,25,.7)'
       }[feature.get('zone_code')]
     })
   })
