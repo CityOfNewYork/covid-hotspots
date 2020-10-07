688f83f8a580178ed377f2f30c0c0fec093522e3
timkeane <t.d.keane@gmail.com>
2020-10-07 09:30:46 -0400

    better presentation of zone info

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import $ from 'jquery'
+
 import nyc from 'nyc-lib/nyc'
 import Basemap from 'nyc-lib/nyc/ol/Basemap'
 import LocationMgr from 'nyc-lib/nyc/ol/LocationMgr'
@@ -29,15 +31,49 @@ var locationMgr = new LocationMgr({
 
 const popup = new Popup({map})
 
+const GUIDANCE = {
+  yellow: {
+    worship: '50% Capacity',
+    mass_gathering: '25 people maximum, indoors and outdoors',
+    business: 'Open',
+    dining: 'Indoor and outdoor dining, 4 person maximum per table',
+    school: 'Open<br>Mandatory weekly testing of students and teachers/staff for in-person settings.'
+  },
+  orange: {
+    worship: '25% Capacity',
+    mass_gathering: '20 people maximum, indoors and outdoors',
+    business: 'Open',
+    dining: 'Indoor and outdoor dining, 4 person maximum per table',
+    school: 'Open<br>Mandatory weekly testing of students and teachers/staff for in-person settings.'
+  },
+  red: {
+    worship: '10% Capacity',
+    mass_gathering: '10 people maximum, indoors and outdoors',
+    business: 'Open',
+    dining: 'Indoor and outdoor dining, 4 person maximum per table',
+    school: 'Open<br>Mandatory weekly testing of students and teachers/staff for in-person settings.'
+  }  
+}
+
+const ACTIVITIES = ['Worship', 'Mass Gathering', 'Business', 'Dining', 'School']
+
 const html = feature => {
   const html = $('<div></div>')
   if (feature) {
-    html.append('<h2>You are located in a risk zone</h2>')
-      .append(nyc.html(feature.getProperties()))
-  } else {
-    html.append('<h2>You are not located in a risk zone</h2>')
+    const zone = feature.get('zone')
+    const guidance = GUIDANCE[zone]
+    const table = $('<table><tbody><tr><th>Activity</th><th>Guidance</th></tr></tbody></table>')
+    const tbody = table.find('tbody')
+    ACTIVITIES.forEach(activity => {
+      const key = activity.replace(/ /, '_').toLowerCase()
+      tbody.append(`<tr><td class="activity">${activity}</td><td>${guidance[key]}</td></tr>`)
+    })
+    html.append(`<h2 class="${zone}" role="alert" aria-live="assertive">You are located in the ${zone} zone</h2>`)
+    .append(table)
+} else {
+    html.append('<h2 role="alert" aria-live="assertive">You are not located in a restricted zone</h2>')
   }
-  return html.append('<a href="https://www1.nyc.gov/site/coronavirus/index.page" target="_blank">More information</a>')
+  return html.append('<a class="btn rad-all info" href="https://www1.nyc.gov/site/coronavirus/index.page" target="_blank">More information</a>')
 }
 
 const located = location => {
@@ -48,6 +84,7 @@ const located = location => {
   } else {
     popup.show({coordinate, html: html()})
   }
+  $('.pop').attr('tab-index', 0).trigger('focus')
 }
 
 new Goog({target: '#map', button: true})
@@ -56,3 +93,5 @@ new Share({target: '#map'})
 map.on('click', located)
 locationMgr.on('geocoded', located)
 locationMgr.on('geolocated', located)
+
+$('.srch input').attr('tab-index', 0).trigger('focus')
--- src/js/style.js
+++ src/js/style.js
@@ -5,9 +5,9 @@ export default (feature, resolution) => {
   return new Style({
     fill: new Fill({
       color: {
-        Red: 'rgba(255,0,0,.7)',
-        Orange: 'rgba(255,165,0,.7)',
-        Yellow: 'rgba(240,255,0,.7)'
+        red: 'rgba(255,0,0,.7)',
+        orange: 'rgba(255,165,0,.7)',
+        yellow: 'rgba(240,255,0,.7)'
       }[feature.get('zone')]
     })
   })
